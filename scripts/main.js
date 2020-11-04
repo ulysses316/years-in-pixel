177e12cf43311b9ca3591714d3ebb3002f11ed3b
ulysses316 <ulises316@live.com.mx>
2020-11-03 23:15:06 -0600

    Ya se cambia el color de cualquier dia

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,37 @@ let today = new Date();
 let day = today.getDate();
 let month = months[today.getMonth()-1];
 
-const focusToday = (day, month)=>{
-    elementMonth = document.querySelector("."+month);
-    elementDay = elementMonth.children[day];
-    elementDay.style.background = "blue"
+elementMonth = document.querySelector("."+month);
+focusDay =  elementMonth.children[day];
+console.log(focusDay);
+
+const emotions = (emotion, focusElement=focusDay)=>{ 
+    switch (emotion) {
+        case 1:
+            focusElement.style.background = "blue";
+            focusElement.dataset.mood = 1;
+            break;
+        case 2:
+            focusElement.style.background = "teal";
+            focusElement.dataset.mood = 2;
+            break;
+        case 3:
+            focusElement.style.background = "yellow";
+            focusElement.dataset.mood = 3;
+            break;
+        case 4:
+            focusElement.style.background = "orange";
+            focusElement.dataset.mood = 4;
+            break;
+        case 5:
+            focusElement.style.background = "red";
+            focusElement.dataset.mood = 5;
+            break;                                    
+    }    
+}
+
+
+const chageFocusDay = ()=>{
+    focusDay = event.target;
 }
 
